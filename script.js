fd1f445fc50c9f6672a99b9824ecc65b491e39b2
SaraCremonesi <saracrem00@gmail.com>
2020-06-10 16:02:24 +0200

    inizio esercizio menu dropdown

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // gestione dell'icona della lingua
+  $(document).on('mouseenter', '.fa-chevron-down'
+  function(
+    $('fa-chevron-up.hidden').removeClass('hidden')
+    $('fa-chevron-down').addClass('hidden')
+  )
+
+});
