848f75996af38ea6baa446260c6f72f87722cd71
SaraCremonesi <saracrem00@gmail.com>
2020-06-10 16:45:20 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
   // gestione dell'icona della lingua
-  $(document).on('mouseenter', '.fa-chevron-down'
-  function(
-    $('fa-chevron-up.hidden').removeClass('hidden')
-    $('fa-chevron-down').addClass('hidden')
-  )
+  $(document).on('mouseenter', '.lang-selector',
+  function() {
+    $('.fa-chevron-up').removeClass('hidden');
+    $('.fa-chevron-down').addClass('hidden');
+  }
+);
+
+$(document).on('mouseleave', '.lang-selector',
+function() {
+  $('.fa-chevron-up').addClass('hidden');
+  $('.fa-chevron-down').removeClass('hidden');
+}
+);
 
 });
