8b9433fc7931122df1619ba92fd5e926d54f9e20
SaraCremonesi <saracrem00@gmail.com>
2020-06-10 20:57:35 +0200

    completamento esercizio

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   function() {
     $('.fa-chevron-up').removeClass('hidden');
     $('.fa-chevron-down').addClass('hidden');
+    $('.dropdown li').this
   }
 );
 
@@ -11,6 +12,19 @@ $(document).on('mouseleave', '.lang-selector',
 function() {
   $('.fa-chevron-up').addClass('hidden');
   $('.fa-chevron-down').removeClass('hidden');
+}
+);
+
+// Gestione dei menu a tendina
+$(document).on('mouseenter', '.with-dropdown',
+function() {
+  $(this).children('.dropdown').toggleClass('active');
+}
+);
+
+$(document).on('mouseleave', '.with-dropdown',
+function() {
+  $(this).children('.dropdown').toggleClass('active');
 }
 );
 
